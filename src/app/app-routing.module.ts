0de4ddea2e58bc29e971c330959671a53c923fd6
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-10-25 13:48:40 -0600

    login and register components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { BooksComponent } from './books/books.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',          component: BooksComponent},
+  {path: 'cart',      component: CartComponent},
+  {path: 'login',     component: LoginComponent},
+  {path: 'register',  component: RegisterComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
