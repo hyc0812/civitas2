d41756ba6a085087140c2f558e1f23d4e3c6d089
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-10-24 23:33:24 -0600

    books file added

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -7,14 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
-  name: string = 'Clean Code';
-  author: string = 'Robert C Martin';
-  src: string = "https://m.media-amazon.com/images/I/51csugYPrJL._SX331_BO1,204,203,200_.jpg";
+  books = [
+    {
+      name: 'Clean Code',
+      author: 'Robert C Martin',
+      src: "https://m.media-amazon.com/images/I/51csugYPrJL._SX331_BO1,204,203,200_.jpg",
+    },
+    {
+      name2: 'Pragmatic Programmer',
+      author2: 'David Thomas',
+      src2: "https://m.media-amazon.com/images/I/91T0FcC2t-L._AC_UL640_FMwebp_QL65_.jpg",
+    }
+  ]
 
-  name2: string = 'Pragmatic Programmer';
-  author2: string = 'David Thomas';
-  src2: string = "https://m.media-amazon.com/images/I/91T0FcC2t-L._AC_UL640_FMwebp_QL65_.jpg";
-  myName: string = '';
+  
+    myName: string = '';
 
 
   isShowing: boolean = true;
